dca13e5bec49b6cf3e1ee723a23178d043a04008
Reniercoetzer92 <reniercoetzer92@gmail.com>
2023-11-20 21:43:06 +0200

    modified:   src/Helpers/Supabase_client.js

--- src/Helpers/Supabase_client.js
+++ src/Helpers/Supabase_client.js
@@ -4,13 +4,13 @@ import { createClient } from '@supabase/supabase-js'
  * The URL of the Supabase project.
  * @type {string}
  */
-const supabaseUrl = 'https://nfbvphgapjsbsmnmqujv.supabase.co'
+const supabaseUrl = 'https://vvsbbsfwkqasdefvfnwf.supabase.co'
 
 /**
  * The Supabase API key for authentication.
  * @type {string}
  */
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mYnZwaGdhcGpzYnNtbm1xdWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYyMzMyMzAsImV4cCI6MjAxMTgwOTIzMH0.BnoPUxHLsE_-hbhAayyG7i0-FLQL-egi8QAwWJi9rOg'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2c2Jic2Z3a3Fhc2RlZnZmbndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA0NzIzNjYsImV4cCI6MjAxNjA0ODM2Nn0.iKpoEmVztLWH-kEmRBSFzxA3WJ_0ijCp90onvyDFSPA'
 
 /**
  * The Supabase client for making API requests to the Supabase project.
